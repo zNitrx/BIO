180273a8fcefce837cd174ae67bf7568c425b0e7
zNitrx <106552682+zNitrx@users.noreply.github.com>
2023-09-02 19:36:24 +0200

    Bio de zNitrx
    La bio de zNitrx, hecha con una plantilla y algnos cambios hechos por mi

---
+++ main.py
@@ -0,0 +1,23 @@
+# Import
+from flask import Flask, render_template,request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app = Flask(__name__)
+
+# Página de contenidos en ejecución
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Habilidades dinámicas
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    return render_template('index.html', button_python=button_python)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
